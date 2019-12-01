7dba9941e9b636d1765c69aa0589c7831798c261
ivanfslee <ivanfslee@gmail.com>
2019-11-30 23:22:27 -0800

    Added componentDidMount and refactored initial state definition

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: '' };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: '' };
+    // }
 
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message }))
     }
 
     render() {
@@ -25,7 +24,7 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat) {
             return <div>Latitude: {this.state.lat}</div>
         }
-        
+
         return <div>Loading...</div>
     };
 }
